c05ead070f11a6adb57cedbefd734307dd4da9ce
Developer Trup <developertrup@gmail.com>
2026-07-31 00:22:05 +0530

    Delete day 2 pratice directory

--- day 2 pratice/test-logic.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main(){
-    int a=20;
-    int b=40;
-  printf("output %d ",(a>b || a==b));
-} 
-
--- day 2 pratice/test2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-main(){
-    int a=5;
-    printf("%d\n",a--);
-    printf("%d",--a);
-}
